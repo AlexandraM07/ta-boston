304c057d83817d69686bb7dcd98e1e2b3aa139d7
AlexandraM07 <110460669+AlexandraM07@users.noreply.github.com>
2022-11-03 19:57:46 +0200

    Host test

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,10 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div>Hello ThreadAffair <i class="fa-solid fa-house"></i></div>
+    <div>Hello ThreadAffair <i className="fa-solid fa-house"></i>
+
+    <img src="/images/logo.jpg" alt="Alt" />
+    </div>
   )
 }
