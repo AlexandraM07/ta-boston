c240623e1d3e8aa564c21d839a296fa64aa719a5
AlexandraM07 <110460669+AlexandraM07@users.noreply.github.com>
2022-11-10 22:07:28 +0200

    ta-boston header-footer

---
+++ components/common/footer/FooterSecondary.jsx
@@ -0,0 +1,11 @@
+export default function FooterSecondary() {
+  return (
+    <>
+      <p className="mb-2">©2019 THREAD AFFAIR. All Rights Reserved.</p>
+      <p className="mb-2">
+        Designed by
+        <img src="/images/logo.jpg" alt="Logo" />
+      </p>
+    </>
+  );
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,21 @@
+import Footer from '../components/common/footer/Footer';
 import Header from '../components/common/header/Header';
+import HomePayment from '../components/homepage/HomePayment';
 
 export default function Home() {
   return (
     <div>
-      <header className="header container mt-5">
+      <header className="header container mt-lg-5">
         <Header></Header>
       </header>
-      <main className="content"></main>
-      <footer className="footer"></footer>
+      <main className="content">
+        <div className="content-main container">
+          <HomePayment></HomePayment>
+        </div>
+      </main>
+      <footer className="footer">
+        <Footer></Footer>
+      </footer>
     </div>
   );
 }
