cc011adcdcd79b6c6ab6df7cf03d74e5751273a1
AlexandraM07 <110460669+AlexandraM07@users.noreply.github.com>
2022-11-21 19:16:18 +0200

    TA-ready

--- components/catalog/ProductTile.jsx
+++ components/catalog/ProductTile.jsx
@@ -1,23 +1,39 @@
+import Link from 'next/link';
+
 export default function ProductTile() {
   return (
     <article className="product-tile">
       <header className="product-tile-header">
         <div className="product-tile-image">
-          <picture>
-            <img src="/images/products/product-01a.jpg" alt="Suit"></img>
-          </picture>
+          <Link href="/" title="Thread affair traveller suit">
+            <picture>
+              <img src="/images/products/product-01a.jpg" alt="Suit"></img>
+            </picture>
+          </Link>
 
           <span className="badge">%</span>
         </div>
 
-        <h1 className="product-tile-title">Thread Affair Traveler Suit</h1>
-        <h2 className="product-tile-subtitle">London fit semi slim</h2>
+        <h1 className="product-tile-title">
+          <Link href="/" title="Thread Affair Traveller Suit">
+            <span> Thread Affair </span>
+            <span> Traveller </span>
+            Suit
+          </Link>
+        </h1>
+        <h2 className="product-tile-subtitle">
+          <Link href="/" title="Thread Affair Traveller Suit">
+            London fit semi slim
+          </Link>
+        </h2>
       </header>
 
       <section className="product-tile-pricing">
         <span className="product-price-regular">399.00</span>
-
-        <span className="product-price">USD 270.00</span>
+        <br />
+        <span className="product-price">
+          USD <span> 270.00 </span>
+        </span>
       </section>
 
       <footer className="product-tile-footer">
